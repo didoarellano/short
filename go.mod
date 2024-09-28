42b03dfd07991197c1b722a1b8dcff9e2ebd61fd
Dido Arellano <didoarellano@gmail.com>
2024-09-28 21:25:49 +1030

    Add user auth

--- go.mod
+++ go.mod
@@ -8,10 +8,22 @@ require (
 )
 
 require (
+	cloud.google.com/go/compute v1.20.1 // indirect
+	cloud.google.com/go/compute/metadata v0.2.3 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/gorilla/context v1.1.1 // indirect
+	github.com/gorilla/mux v1.6.2 // indirect
+	github.com/gorilla/securecookie v1.1.1 // indirect
+	github.com/gorilla/sessions v1.2.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/markbates/goth v1.80.0 // indirect
+	github.com/rbcervilla/redisstore/v8 v8.1.0 // indirect
 	golang.org/x/crypto v0.27.0 // indirect
+	golang.org/x/oauth2 v0.17.0 // indirect
 	golang.org/x/text v0.18.0 // indirect
+	google.golang.org/appengine v1.6.8 // indirect
+	google.golang.org/protobuf v1.32.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,52 +3,118 @@ package main
 import (
 	"context"
 	"embed"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/go-redis/redis/v8"
-	"github.com/jackc/pgx/v5"
+	"github.com/gorilla/mux"
+	"github.com/markbates/goth"
+	"github.com/markbates/goth/gothic"
+	"github.com/markbates/goth/providers/google"
+	"github.com/rbcervilla/redisstore/v8"
 )
 
 //go:embed templates/*
 var resources embed.FS
 var t = template.Must(template.ParseFS(resources, "templates/*"))
 
+var sessionStore *redisstore.RedisStore
+
+func oAuthCallbackHandler(w http.ResponseWriter, r *http.Request) {
+	gothUser, err := gothic.CompleteUserAuth(w, r)
+	if err != nil {
+		http.Error(w, "Authentication failed: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// TODO Save user to db
+
+	session, _ := sessionStore.Get(r, "auth")
+	session.Values["id"] = gothUser.UserID
+	session.Values["username"] = gothUser.NickName
+	err = session.Save(r, w)
+
+	if err != nil {
+		log.Fatal(err)
+		http.Error(w, "Failed to set session", http.StatusInternalServerError)
+		return
+	}
+
+	http.Redirect(w, r, "/dashboard", http.StatusSeeOther)
+}
+
+func signinHandler(w http.ResponseWriter, r *http.Request) {
+	session, _ := sessionStore.Get(r, "auth")
+	if session.Values["id"] != nil {
+		http.Redirect(w, r, "/dashboard", http.StatusFound)
+		return
+	}
+	if err := t.ExecuteTemplate(w, "signin.html", nil); err != nil {
+		http.Error(w, "Failed to render template", http.StatusInternalServerError)
+	}
+}
+
+func logoutHandler(w http.ResponseWriter, r *http.Request) {
+	session, _ := sessionStore.Get(r, "auth")
+	session.Options.MaxAge = -1
+	err := session.Save(r, w)
+
+	if err != nil {
+		log.Fatal("Failed to delete session", err)
+		return
+	}
+
+	err = gothic.Logout(w, r)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Logout failed: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
 func main() {
-	opt, _ := redis.ParseURL(os.Getenv("REDIS_URL"))
-	rdb := redis.NewClient(opt)
-
-	pg_conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer pg_conn.Close(context.Background())
-
-	_, err = rdb.Ping(context.Background()).Result()
+	opt, err := redis.ParseURL(os.Getenv("REDIS_URL"))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	var greeting string
-	err = pg_conn.QueryRow(context.Background(), "select 'Hello, world!'").Scan(&greeting)
+	redis_client := redis.NewClient(opt)
+
+	sessionStore, err = redisstore.NewRedisStore(context.Background(), redis_client)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Faled to create redis store", err)
 	}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	goth.UseProviders(
+		google.New(
+			os.Getenv("GOOGLE_CLIENT_ID"),
+			os.Getenv("GOOGLE_CLIENT_SECRET"),
+			os.Getenv("GOOGLE_REDIRECT_URL"),
+		),
+	)
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		data := map[string]interface{}{
-			"greeting": greeting,
+			"greeting": "Hello world",
 		}
 		if err := t.ExecuteTemplate(w, "index.html", data); err != nil {
 			http.Error(w, "Failed to render template", http.StatusInternalServerError)
 		}
-	})
+	}).Methods("GET")
+	r.HandleFunc("/signin", signinHandler).Methods("GET")
+	r.HandleFunc("/logout", logoutHandler).Methods("POST")
+	r.HandleFunc("/auth/{provider}", gothic.BeginAuthHandler).Methods("GET")
+	r.HandleFunc("/auth/{provider}/callback", oAuthCallbackHandler).Methods("GET")
 
 	port, exists := os.LookupEnv("PORT")
 	if !exists {
 		port = "8080"
 	}
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Println("Server started on port " + port)
+	log.Fatal(http.ListenAndServe(":"+port, r))
 }
