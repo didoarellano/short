9eebd4c6630a2a992287e854dbabdf86eddd5b5a
Dido Arellano <didoarellano@gmail.com>
2024-10-09 20:44:43 +1030

    Factor out links handlers

--- main.go
+++ main.go
@@ -4,21 +4,17 @@ import (
 	"context"
 	"embed"
 	"encoding/gob"
-	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
-	"strconv"
-	"strings"
 
 	"github.com/didoarellano/short/internal/auth"
 	"github.com/didoarellano/short/internal/db"
-	"github.com/didoarellano/short/internal/shortcode"
+	"github.com/didoarellano/short/internal/links"
 	"github.com/go-redis/redis/v8"
 	"github.com/gorilla/mux"
 	"github.com/jackc/pgx/v5"
-	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/markbates/goth"
 	"github.com/markbates/goth/gothic"
 	"github.com/markbates/goth/providers/google"
@@ -32,236 +28,9 @@ var t = template.Must(template.ParseFS(resources, "templates/*"))
 var queries *db.Queries
 var sessionStore *redisstore.RedisStore
 
-const paginationLimit int = 2
-
-type PaginationLink struct {
-	Href     string
-	Text     string
-	Disabled bool
-}
-type PaginationLinks []PaginationLink
-
-func userLinksHandler(w http.ResponseWriter, r *http.Request) {
-	session, _ := sessionStore.Get(r, "session")
-	user := session.Values["user"].(auth.UserSession)
-	userID := user.UserID
-
-	// No page query param defaults to page 1
-	currentPage := 1
-	if pageParam := r.URL.Query().Get("page"); pageParam != "" {
-		if parsedPage, err := strconv.Atoi(pageParam); err == nil {
-			currentPage = parsedPage
-		}
-	}
-
-	if currentPage < 1 {
-		http.Redirect(w, r, "/links", http.StatusSeeOther)
-		return
-	}
-
-	links, err := queries.GetPaginatedLinksForUser(context.Background(), db.GetPaginatedLinksForUserParams{
-		UserID: userID,
-		Limit:  int32(paginationLimit),
-		Offset: int32((currentPage - 1) * paginationLimit),
-	})
-
-	if err != nil {
-		log.Printf("Failed to retrieve user's links: %v", err)
-		http.Error(w, "Failed to retrieve user's links: %v", http.StatusInternalServerError)
-		return
-	}
-
-	totalPages := (int(links.TotalCount) + paginationLimit - 1) / paginationLimit
-
-	if currentPage > totalPages {
-		http.Redirect(w, r, fmt.Sprintf("/links?page=%d", totalPages), http.StatusSeeOther)
-		return
-	}
-
-	paginationLinks := PaginationLinks{
-		{
-			Text:     "first",
-			Href:     "/links",
-			Disabled: currentPage == 1,
-		},
-		{
-			Text:     "prev",
-			Href:     fmt.Sprintf("/links?page=%d", currentPage-1),
-			Disabled: currentPage == 1,
-		},
-		{
-			Text:     "next",
-			Href:     fmt.Sprintf("/links?page=%d", currentPage+1),
-			Disabled: currentPage == totalPages,
-		},
-		{
-			Text:     "last",
-			Href:     fmt.Sprintf("/links?page=%d", totalPages),
-			Disabled: currentPage == totalPages,
-		},
-	}
-
-	data := map[string]interface{}{
-		"user":            user,
-		"links":           links.Links,
-		"paginationLinks": paginationLinks,
-	}
-
-	if err := t.ExecuteTemplate(w, "links.html", data); err != nil {
-		http.Error(w, "Failed to render template", http.StatusInternalServerError)
-	}
-}
-
-type FormData struct {
-	DestinationUrl  string
-	Title           string
-	Notes           string
-	CreateDuplicate bool
-}
-
-type DuplicateUrl struct {
-	Text string
-	Href string
-}
-
-type DuplicateUrls struct {
-	Urls           []DuplicateUrl
-	DestinationUrl string
-	RemainingCount int32
-}
-
-type FormFieldValidation struct {
-	Message   string
-	Value     string
-	IsChecked bool
-}
-
-type FormValidationErrors struct {
-	FormFields map[string]FormFieldValidation
-	Duplicates DuplicateUrls
-}
-
-func CreateLinkHandler(w http.ResponseWriter, r *http.Request) {
-	var validationErrors FormValidationErrors
-	session, _ := sessionStore.Get(r, "session")
-
-	if r.Method == "GET" {
-		flashes := session.Flashes()
-		if len(flashes) > 0 {
-			if v, ok := flashes[0].(FormValidationErrors); ok {
-				validationErrors = v
-			}
-		}
-		data := map[string]interface{}{
-			"validationErrors": validationErrors,
-		}
-		session.Save(r, w)
-		if err := t.ExecuteTemplate(w, "create_link.html", data); err != nil {
-			http.Error(w, "Failed to render template", http.StatusInternalServerError)
-		}
-		return
-	}
-
-	user := session.Values["user"].(auth.UserSession)
-	userID := user.UserID
-
-	r.ParseForm()
-	formData := FormData{
-		DestinationUrl:  strings.TrimSpace(r.FormValue("url")),
-		Title:           strings.TrimSpace(r.FormValue("title")),
-		Notes:           strings.TrimSpace(r.FormValue("notes")),
-		CreateDuplicate: r.FormValue("create-duplicate") == "on",
-	}
-
-	if formData.DestinationUrl == "" {
-		validationErrors := FormValidationErrors{
-			FormFields: map[string]FormFieldValidation{
-				"Url": {
-					Value:   formData.DestinationUrl,
-					Message: "Destination URL is required",
-				},
-				"Title": {
-					Value: formData.Title,
-				},
-				"Notes": {
-					Value: formData.Notes,
-				},
-				"CreateDuplicate": {
-					IsChecked: formData.CreateDuplicate,
-				},
-			},
-		}
-		session.AddFlash(validationErrors)
-		session.Save(r, w)
-		http.Redirect(w, r, "/links/new", http.StatusFound)
-		return
-	}
-
-	if !formData.CreateDuplicate {
-		links, _ := queries.FindDuplicatesForUrl(context.Background(), db.FindDuplicatesForUrlParams{
-			UserID:         userID,
-			DestinationUrl: formData.DestinationUrl,
-			Limit:          3,
-		})
-
-		if len(links.ShortCodes) > 0 {
-			dupes := DuplicateUrls{
-				DestinationUrl: formData.DestinationUrl,
-				RemainingCount: links.RemainingCount,
-			}
-			for _, shortcode := range links.ShortCodes {
-				url := DuplicateUrl{
-					Href: "/links/" + shortcode,
-					Text: shortcode,
-				}
-				dupes.Urls = append(dupes.Urls, url)
-			}
-			validationErrors := FormValidationErrors{
-				Duplicates: dupes,
-				FormFields: map[string]FormFieldValidation{
-					"Url": {
-						Value: formData.DestinationUrl,
-					},
-					"Title": {
-						Value: formData.Title,
-					},
-					"Notes": {
-						Value: formData.Notes,
-					},
-					"CreateDuplicate": {
-						IsChecked: formData.CreateDuplicate,
-					},
-				},
-			}
-			session.AddFlash(validationErrors)
-			session.Save(r, w)
-			http.Redirect(w, r, "/links/new", http.StatusSeeOther)
-			return
-		}
-	}
-
-	shortCode := shortcode.New(userID, formData.DestinationUrl, 7)
-
-	_, err := queries.CreateLink(context.Background(), db.CreateLinkParams{
-		UserID:         userID,
-		ShortCode:      shortCode,
-		DestinationUrl: formData.DestinationUrl,
-		Title:          pgtype.Text{String: formData.Title, Valid: true},
-		Notes:          pgtype.Text{String: formData.Notes, Valid: true},
-	})
-
-	if err != nil {
-		log.Printf("Failed to create new link: %v", err)
-		http.Error(w, "Failed to create new link", http.StatusInternalServerError)
-		return
-	}
-
-	http.Redirect(w, r, "/links", http.StatusSeeOther)
-}
-
 func main() {
 	gob.Register(auth.UserSession{})
-	gob.Register(FormValidationErrors{})
+	gob.Register(links.FormValidationErrors{})
 	ctx := context.Background()
 
 	opt, err := redis.ParseURL(os.Getenv("REDIS_URL"))
@@ -312,8 +81,12 @@ func main() {
 	})
 
 	// Private routes
-	r.Handle("/links", privateRoute(http.HandlerFunc(userLinksHandler))).Methods("GET")
-	r.Handle("/links/new", privateRoute(http.HandlerFunc(CreateLinkHandler))).Methods("GET", "POST")
+	r.Handle("/links", privateRoute(http.HandlerFunc(
+		links.UserLinksHandler(t, queries, sessionStore),
+	))).Methods("GET")
+	r.Handle("/links/new", privateRoute(http.HandlerFunc(
+		links.CreateLinkHandler(t, queries, sessionStore),
+	))).Methods("GET", "POST")
 
 	port, exists := os.LookupEnv("PORT")
 	if !exists {
